a38d9698d649869d63d0c6080499193ecf94f90f
karthikp249 <karthikp249@gmail.com>
2021-11-25 21:56:14 +0530

    'assessment'

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const { Worker } = require('worker_threads');
+
+/* const temp = require('./temp'); */
+
+let count = 0;
+app.get('/',(req,res)=>{
+    count += 1;
+    res.status(200).json({countter : count});
+});
+
+app.get('/heavy',(req,res)=>{
+
+    const worker = new Worker('./worker');
+    worker.on('message',(data)=>{
+        res.status(200).json({total : data});
+    });
+});
+
+app.listen(port,()=>{
+    console.log(`App listening at http://localhost:${port}`)
+});
